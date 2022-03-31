57bd5a2491271824a9746e0bab1f42b092eac09e
bselee <bselee@clemson.edu>
2022-03-31 12:13:47 -0400

    Working on implementing and understanding aspp

--- resnet.py
+++ resnet.py
@@ -7,9 +7,66 @@ or 'BasicBlock' as they use this instead of the 'BottleNeck' class.
 ResNet Paper: https://arxiv.org/pdf/1512.03385.pdf
 PyTorch ResNet w/ Dilation: https://github.com/pytorch/vision/blob/main/torchvision/models/resnet.py 
 """
+from os import defpath
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+
+class ASPP(nn.module):
+
+    def __init__(self, C, depth, num_classes, conv=nn.Conv2d, norm=nn.BatchNorm2d, momentum=0.0003, mult=1):
+        """
+        Args:
+            C: number of in_channels
+            depth: number of out_channels
+        """
+        super().init()
+        self._C = C
+        self._depth = depth
+        self._num_classes=num_classes
+#########################################################################################finish aspp implementation and understanding
+        # Global average pooling is used to help the problem of losing information
+        # when atrous rates are large enough, close to the size of the feature map.
+        # This is explained in the deeplabv3 paper section 3.3
+        self.global_pooling = nn.AdaptiveAvgPooling2d(1) # Global pooling, output size 1
+        self.relu = nn.ReLU(inplace=True)
+        # Defining convolutions with atrous rates [1, 6, 12, 18]
+        self.aspp1 = conv(C,depth, kernel_size=1, stride=1, bias=False)
+        self.aspp2 = conv(C, depth, kernel_size=3, stride=1,
+                          dilation=int(6*mult), padding=int(6*mult), bias=False)
+        self.aspp3 = conv(C, depth, kernel_size=3, stride=1,
+                          dilation=int(12*mult), padding=int(12*mult), bias=False)
+        self.aspp4 = conv(C, depth, kernel_size=3, stride=1,
+                          dilation=int(18*mult), padding=int(12*mult), bias=False)
+        self.aspp5 = conv(C, depth, kernel_size=1, stride=1, bias=False)
+        self.aspp1_bn = norm(depth, momentum)
+        self.aspp2_bn = norm(depth, momentum)
+        self.aspp3_bn = norm(depth, momentum)
+        self.aspp4_bn = norm(depth, momentum)
+        self.aspp5_bn = norm(depth, momentum)
+        self.conv2 = conv(depth*5, depth, kernel_size=1, stirde=1, bias=False)
+        self.bn2 = norm(depth, momentum)
+        self.conv3 = nn.Conv2d(depth, num_classes, kernel_size=1, stride=1)
+    
+    def forward(self, x):
+        x1 = self.aspp1(x)
+        x1 = self.aspp1_bn(x1)
+        x1 = self.relu(x1)
+        x2 = self.aspp2(x)
+        x2 = self.aspp2_bn(x2)
+        x2 = self.relu(x2)
+        x3 = self.aspp3(x)
+        x3 = self.aspp3_bn(x3)
+        x3 = self.relu(x3)
+        x4 = self.aspp4(x)
+        x4 = self.aspp4_bn(x4)
+        x4 = self.relu(x4)
+        x5 = self.global_pooling(x)
+        x5 = self.aspp5(x5)
+        x5 = self.aspp5_bn(x5)
+        x5 = self.relu(x5)
+
 
 
 class BottleNeck():
@@ -32,6 +89,14 @@ class ResNet(nn.module):
         self.conv = nn.Conv2d # Mostly use for 1x1 convolutions
         self.norm_layer = nn.BatchNorm2d
 
+        if replace_stride_with_dilation is None:
+            # each element in the list indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            # for deeplab you want the third element to be true,
+            # for the normal resnet without dilation they all should be false
+            # I think. This should be set when calling ResNet#########################################################
+            replace_stride_with_dilation = [False, True, True]
+
         # No bias because batch normalization, next layer, takes care of it
         # Padding and stride values set of the first conv layer set by ResNet paper
         # This first layer downsamples the input by 2
@@ -40,14 +105,15 @@ class ResNet(nn.module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True) # inplace can save a small amount of memory by not creating a new object
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1,
-                               padding=1, bias=False)
+        #self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1,
+        #                       padding=1, bias=False)
         self.layer1 = self._make_layer(block, 64, layers[0]) # defining conv blocks after the first conv layer
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0]) # start downsampling here => stride=2
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
          # Apply dilation only for block 4. If replace_stride_with_dilation[i]=True,
          # _make_layer() will set to stride to 1 and dilation will equal the stride argument value, this case 2
+        self.aspp = ASPP(512*block.expansion, 256, num_classes, conv=self.conv, norm=self.norm)
 
     def _make_layer(self, block, planes, blocks, stride=1, dilate=False):
         """Create convolutional blocks w/ hyperparameters specified by resnet.
