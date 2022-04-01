3a788f232188c0a61461f2c25734849259e51838
bselee <bselee@clemson.edu>
2022-04-01 11:48:00 -0400

    finished aspp class

--- resnet.py
+++ resnet.py
@@ -25,13 +25,14 @@ class ASPP(nn.module):
         self._C = C
         self._depth = depth
         self._num_classes=num_classes
-#########################################################################################finish aspp implementation and understanding
+        # ##### need to look for the upsampling part in the pytorch github to verify##### #
         # Global average pooling is used to help the problem of losing information
         # when atrous rates are large enough, close to the size of the feature map.
-        # This is explained in the deeplabv3 paper section 3.3
+        # This is explained in the deeplabv3 paper section 3.3. This is  performed 
+        # in the x5 step
         self.global_pooling = nn.AdaptiveAvgPooling2d(1) # Global pooling, output size 1
         self.relu = nn.ReLU(inplace=True)
-        # Defining convolutions with atrous rates [1, 6, 12, 18]
+        # Defining convolutions with atrous rates [6, 12, 18]
         self.aspp1 = conv(C,depth, kernel_size=1, stride=1, bias=False)
         self.aspp2 = conv(C, depth, kernel_size=3, stride=1,
                           dilation=int(6*mult), padding=int(6*mult), bias=False)
@@ -45,11 +46,12 @@ class ASPP(nn.module):
         self.aspp3_bn = norm(depth, momentum)
         self.aspp4_bn = norm(depth, momentum)
         self.aspp5_bn = norm(depth, momentum)
-        self.conv2 = conv(depth*5, depth, kernel_size=1, stirde=1, bias=False)
+        self.conv2 = conv(depth*5, depth, kernel_size=1, stride=1, bias=False)
         self.bn2 = norm(depth, momentum)
         self.conv3 = nn.Conv2d(depth, num_classes, kernel_size=1, stride=1)
     
     def forward(self, x):
+        # perform 5 different convolutions, each using the orignal data x
         x1 = self.aspp1(x)
         x1 = self.aspp1_bn(x1)
         x1 = self.relu(x1)
@@ -62,10 +64,30 @@ class ASPP(nn.module):
         x4 = self.aspp4(x)
         x4 = self.aspp4_bn(x4)
         x4 = self.relu(x4)
+        # x5 is used to incorporate global context information
+        # AdapativeAvgPooling(1) returns only 1 pixel from each feature map 
+        # so they need to be upsampled to the original feature map size of x
         x5 = self.global_pooling(x)
         x5 = self.aspp5(x5)
         x5 = self.aspp5_bn(x5)
         x5 = self.relu(x5)
+        # Upsampling to orignal feature map (x) size
+        x5 = nn.Upsample((x.shape[2], x.shape[3]), mode='bilinear', 
+                         align_corners=True)(x5) # shape[2]=height, shape[3]=width
+        # Concatenate feature maps from each convolution.
+        # This will append feature maps on the 1st dimension which effectively 'stacks'
+        # them like normal Conv2d with multiple output channels.
+        # The feature maps are of size [batch_size, num_channels, height, width]
+        # so dim=1 will append the feature maps to the num_channels dimension
+        x = torch.cat((x1, x2, x3, x4 ,x5), dim=1) 
+        # Fuse concatenated feature maps using 1x1 convolutions
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+        # General the final logits using 1x1 convolution
+        x = self.conv3(x)
+        
+        return x
 
 
 
@@ -74,7 +96,7 @@ class BottleNeck():
     Each BottleNeck has 3 layers instead of 2. The 3 layers are 1x1, 3x3, and 1x1 convolutions.
     The 1x1 layers are responsible for reducing and then increasing (retoring) dimensions
     
-    """"
+    """
 
     # Value to multiply the output channels by in the last 1x1 BottleNeck layer (Fig. 5 & Table 1)
     expansion = 4  
